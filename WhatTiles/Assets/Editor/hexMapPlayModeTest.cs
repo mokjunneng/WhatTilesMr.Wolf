22379174ab2323e135b4fb984473c6b6211a6258
Kimberlyn <bobokimberlyn@gmail.com>
2018-03-16 09:59:23 +0800

    added

---
+++ WhatTiles/Assets/Editor/hexMapPlayModeTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class hexMapPlayModeTest {
+
+    [Test]
+    public void hexMapTestingGameObject()
+    {
+        // Use the Assert class to test conditions.
+
+        var go = GameObject.FindGameObjectWithTag("TileMap");
+        Assert.AreEqual("HexMap", go.GetComponent<HexMap>().name);
+    }
+
+    //check for correct total number of tiles (correct initialisation of map) => 0 since game havent run
+    [Test]
+    public void hexMapTestingTotalTiles()
+    {
+        var go = GameObject.FindGameObjectWithTag("TileMap");
+        Assert.AreEqual(0, go.GetComponent<HexMap>().totalCount);
+    }
+
+    //check for correct total number of tiles (correct initialisation of map) => 0 since game havent run 
+    [Test]
+    public void hexMapTestingPlayerTiles()
+    {
+        var go = GameObject.FindGameObjectWithTag("TileMap");
+        var player = GameObject.FindGameObjectWithTag("Player");
+        Assert.AreEqual(0, go.GetComponent<HexMap>().playerCount);
+    }
+
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+	public IEnumerator hexMapPlayModeTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+}
--- WhatTiles/Assets/Editor/hexMapTesting.cs
+++ WhatTiles/Assets/Editor/hexMapTesting.cs
@@ -37,7 +37,7 @@ public class hexMapTesting
     // A UnityTest behaves like a coroutine in PlayMode
     // and allows you to yield null to skip a frame in EditMode
     [UnityTest]
-    public IEnumerator signInTestingWithEnumeratorPasses()
+    public IEnumerator hexMapTestingWithEnumeratorPasses()
     {
         // Use the Assert class to test conditions.
         // yield to skip a frame
