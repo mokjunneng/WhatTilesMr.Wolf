a9914e58d78f70369889ecb72bb7fdfd661e392a
testkoyetw <23208882+testkoyetw@users.noreply.github.com>
2018-04-12 20:50:04 +0800

    lobby screen & removable pop up menu

---
+++ whatTiles/Assets/Scripts/PanelMaskScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PanelMaskScript : MonoBehaviour, IPointerDownHandler
+{
+    public GameObject hostPopUp;
+    public GameObject roomPopUp;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        hostPopUp.SetActive(false);
+        roomPopUp.SetActive(false);
+    }
+
+}
